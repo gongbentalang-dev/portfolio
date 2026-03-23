827c61ed212b60ee7ffc82aa4e5de1b0316afae0
gongbentalang-dev <gongbentalang@gmail.com>
2026-03-23 14:15:24 +0900

    整理

--- js/slideshow.js
+++ js/slideshow.js
@@ -1,55 +1,52 @@
 const images = [
-"images/photo1.jpg",
-"images/photo2.jpg",
-"images/photo3.jpg",
-"images/photo4.jpg",
-"images/photo5.jpg",
-"images/photo6.jpg",
-"images/photo7.jpg",
-"images/photo8.jpg",
-"images/photo9.jpg",
-"images/photo10.jpg",
-"images/photo11.jpg",
-"images/photo12.jpg",
-"images/photo13.jpg",
-"images/photo14.jpg",
-"images/photo15.jpg",
-"images/photo16.jpg",
-"images/photo17.jpg",
-"images/photo18.jpg",
+  "images/photo1.jpg",
+  "images/photo2.jpg",
+  "images/photo3.jpg",
+  "images/photo4.jpg",
+  "images/photo5.jpg",
+  "images/photo6.jpg",
+  "images/photo7.jpg",
+  "images/photo8.jpg",
+  "images/photo9.jpg",
+  "images/photo10.jpg",
+  "images/photo11.jpg",
+  "images/photo12.jpg",
+  "images/photo13.jpg",
+  "images/photo14.jpg",
+  "images/photo15.jpg",
+  "images/photo16.jpg",
+  "images/photo17.jpg",
+  "images/photo18.jpg",
 ];
 
-let index = 0;
+let currentIndex = 0;
 
 const slide1 = document.getElementById("slide1");
 const slide2 = document.getElementById("slide2");
 
-let showingFirst = true;
+let isFirstVisible = true;
 
-function changeSlide(){
+// 初期画像セット
+slide1.src = images[currentIndex];
 
-index++;
+// スライド更新
+function updateSlide() {
+  currentIndex = (currentIndex + 1) % images.length;
 
-if(index >= images.length){
-index = 0;
+  const currentImage = images[currentIndex];
+
+  if (isFirstVisible) {
+    slide2.src = currentImage;
+    slide2.classList.add("active");
+    slide1.classList.remove("active");
+  } else {
+    slide1.src = currentImage;
+    slide1.classList.add("active");
+    slide2.classList.remove("active");
+  }
+
+  isFirstVisible = !isFirstVisible;
 }
 
-if(showingFirst){
-
-slide2.src = images[index];
-slide2.classList.add("active");
-slide1.classList.remove("active");
-
-}else{
-
-slide1.src = images[index];
-slide1.classList.add("active");
-slide2.classList.remove("active");
-
-}
-
-showingFirst = !showingFirst;
-
-}
-
-setInterval(changeSlide,4000);
+// スライド開始
+setInterval(updateSlide, 4000);
